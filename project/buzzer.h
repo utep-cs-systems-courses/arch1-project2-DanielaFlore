6f0c24d9b39def4974e4432de8afbbbf8ee71d78
DanielaFlore <dflores44@miners.utep.edu>
2020-04-14 22:16:54 -0600

    deleted redundant code

--- project/buzzer.h
+++ project/buzzer.h
@@ -12,12 +12,3 @@ void buzzer_set_period(short cycles);
 
 
 #endif // included
-/*
-#ifndef buzzer_included
-#define buzzer_included
-
-void buzzer_init();
-void buzzer_set_period(short cycles);
-
-#endif // included
-*/
